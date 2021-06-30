fc6b6bfb08b4761acac6b705c638545947ef6577
imtiaj <imtiajahmed723@gmail.com>
2021-06-30 22:41:54 +0600

    crud operation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
 require('dotenv').config()
 const ObjectId = require('mongodb').MongoClient
 const { MongoClient } = require('mongodb');
@@ -8,6 +10,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const app = express();
 app.use(cors());
+app.use(express.static('blogs'))
+app.use(fileUpload())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -24,18 +28,37 @@ client.connect(err =>{
 
     // post blog from here .....
     app.post('/post-blog', (req, res) =>{
-        const postedBlog = req.body
-        blogPost.insertOne(postedBlog)
-        .then(res =>{
-            console.log(res.insertedCount)
-            res.send(res.insertedCount > 0)
+        const file = req.files.file;
+        const blogTitle = req.body.blogTitle;
+        const postOwner = req.body.postOwner;
+        const blogDescription = req.body.blogDescription;
+        const category = req.body.category;
+
+        const newImage = file.data
+        const convertedImage = newImage.toString("base64")
+        const image = {
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer.from(convertedImage, "base64")
+        }        
+
+        blogPost.insertOne({blogTitle, postOwner, blogDescription, category, image})
+        .then(result =>{
+            fs.remove(filePath, error =>{
+                if (error){
+                    console.log(error)
+                    res.status(500).send({ msg: "Failed to upload Image" });
+                }
+                console.log(result.insertedCount > 0)
+                res.send(result.insertedCount > 0)
+            })
         })
     })
 
+
     // get blog from here .....
     app.get('/blogs', (req, res) =>{
-        blogPost.find()
-        .toArray((err, items) =>{
+        blogPost.find().toArray((err, items) =>{
             res.send(items)
         })
     })
